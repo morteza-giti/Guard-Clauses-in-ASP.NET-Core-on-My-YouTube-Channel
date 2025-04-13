ea666bb36034d5e6cfbcec8379a5f6283a166167
Pirooz <mortezagiti1990@gmail.com>
2025-04-13 09:19:04 -0400

    end of episode 1

---
+++ GuardClausesInASPNETCore/Order.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.GuardClauses;
+
+namespace GuardClausesInASPNETCore
+{
+    public class Order
+    {
+        public string CustomerEmail { get; set; }
+        public string? OrderNumber { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public Order(string customerEmail, string? orderNumber, DateTime? orderData)
+        {
+            CustomerEmail = Guard.Against.NullOrEmpty(customerEmail, nameof(customerEmail), "Customer email address cannot be null or empty.");
+            OrderNumber = orderNumber;
+            OrderDate = orderData;
+        }
+    }
+}
---
+++ GuardClausesInASPNETCore/OrderProcessingService.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.GuardClauses;
+
+namespace GuardClausesInASPNETCore
+{
+    public interface IPaymentGateway
+    {
+
+    }
+    public interface IOrderRepository
+    {
+
+    }
+    public class OrderProcessingService
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IPaymentGateway paymentGateway;
+
+        public OrderProcessingService(IOrderRepository orderRepository, IPaymentGateway paymentGateway)
+        {
+            //if (orderRepository == null)
+            //{
+            //    throw new ArgumentException("Order Repository cannot be null");
+            //}
+            //if (paymentGateway == null)
+            //{
+            //    throw new ArgumentException("Payment Gateway cannot be null");
+            //}
+
+            this.orderRepository = Guard.Against.Null(orderRepository, nameof(orderRepository));
+            this.paymentGateway = Guard.Against.Null(paymentGateway, nameof(paymentGateway));
+        }
+    }
+}
